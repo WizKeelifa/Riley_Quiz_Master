77fe01b58083506138d04875880339736c6d2930
Keeley Riley <riley291@morrisville.edu>
2022-04-05 15:25:27 -0400

    Keeping Score

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -25,9 +25,14 @@ public class Quiz : MonoBehaviour
     [SerializeField] Image timerImage;
     Timer timer;
 
+    [Header("Scoring")]
+    [SerializeField] TextMeshProUGUI scoreText;
+    ScoreKeeper scoreKeeper;
+
     void Start()
     {
         timer = FindObjectOfType<Timer>();
+        scoreKeeper = FindObjectOfType<ScoreKeeper>();
     }
 
     void Update()
@@ -52,6 +57,7 @@ public class Quiz : MonoBehaviour
         DisplayAnswer(index);
         SetButtonState(false);
         timer.CancelTimer();
+        scoreText.text = "Score" + scoreKeeper.CalculateScore() + "%";
     }
 
     void DisplayAnswer(int index)
@@ -63,6 +69,7 @@ public class Quiz : MonoBehaviour
             questionText.text = "Correct!";
             buttonImage = answerButtons[index].GetComponent<Image>();
             buttonImage.sprite = correctAnswerSprite;
+            scoreKeeper.IncrementCorrectAnswers();
         }
         else
         {
@@ -83,7 +90,8 @@ public class Quiz : MonoBehaviour
             SetButtonState(true);
             SetDefaultButtonSprites();
             GetRandomQuestion();
-            DisplayQuestion();            
+            DisplayQuestion();
+            scoreKeeper.IncrementQuestionsSeen();           
         }
 
     }
